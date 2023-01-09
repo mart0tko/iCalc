8a30b147b438aad0afdc8bc98bbdb21257f86da9
Martin Georgiev <mgeorgiev@aiopsgroup.com>
2023-01-09 20:45:48 +0200

    Test 2

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    locales: ["en", "ar"],
+    // locales: ["en", "ar"],
+    locales: ["en"],
     defaultLocale: "en",
     localeDetection: false,
     localePath: path.resolve("./public/locales"),
