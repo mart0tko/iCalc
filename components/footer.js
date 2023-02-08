de17cd9181b9f3afc86c9df142dcffd1f893cace
Martin Georgiev <mgeorgiev@aiopsgroup.com>
2023-02-08 10:33:38 +0200

    Footer updated

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import { Link, Paper, Typography } from "@mui/material";
+import { Icon } from "@mui/material";
 
 export default function Footer() {
   return (
@@ -16,32 +17,79 @@ export default function Footer() {
       component="footer"
       variant="outlined"
     >
-      <Link
-        href="/privacy-policy"
-        sx={{
-          color: "initial",
-          textDecorationColor: "initial",
-          cursor: "pointer",
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        Privacy Policy
-      </Link>
-      {" | "}
-      <Link
-        href="/terms-of-use"
-        sx={{
-          color: "initial",
-          textDecorationColor: "initial",
-          cursor: "pointer",
-        }}
-      >
-        Terms of Use
-      </Link>
-      <br />
+        <div
+          style={{
+            display: "flex",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "canter",
+            flexDirection: "column",
+            padding: "1rem",
+          }}
+        >
+          <Link
+            href="/privacy-policy"
+            sx={{
+              color: "initial",
+              textDecorationColor: "initial",
+              cursor: "pointer",
+              marginBottom: "0.5rem",
+            }}
+          >
+            Privacy Policy
+          </Link>
+          <Link
+            href="/terms-of-use"
+            sx={{
+              color: "initial",
+              textDecorationColor: "initial",
+              cursor: "pointer",
+            }}
+          >
+            Terms of Use
+          </Link>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flex: 1,
+            justifyContent: "center",
+          }}
+        >
+          <Link
+            href="https://www.facebook.com/profile.php?id=100090344551476"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center ",
+            }}
+          >
+            <Icon
+              sx={{ mb: 1 }}
+              style={{
+                fontSize: "25px",
+                marginRight: "0.5rem",
+                marginBottom: 0,
+              }}
+            >
+              facebook
+            </Icon>
+            Follow Us
+          </Link>
+        </div>
+      </div>
       <Typography
         variant="caption"
         color="initial"
-        sx={{ marginRight: "1rem" }}
+        sx={{ marginRight: "1rem", borderTop: "1px solid dotted" }}
       >
         © Copyright 2023 WannaCalc. All Rights Reserved.
       </Typography>
