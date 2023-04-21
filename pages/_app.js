53ea37a35dc14c9b3234004fd0107dab38b34267
Martin Georgiev <mgeorgiev@aiopsgroup.com>
2023-04-21 14:13:41 +0300

    Vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,14 @@ import Header from "../components/Header";
 import Footer from "../components/footer";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
+import { Analytics } from "@vercel/analytics/react";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GoogleAnalytics />
       <Header>
         <Component {...pageProps} />
+        <Analytics />
       </Header>
       <Footer />
     </>
