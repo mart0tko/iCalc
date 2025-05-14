d7558f67be228c34052014dd3d6db106c6ee1583
Martin Georgiev <m.georgievv@gmail.com>
2025-05-14 23:42:50 +0300

    Tag manager added

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,12 @@ class MyDocument extends Document {
         lang={this.props.locale}
       >
         <Head>
+        {/* Google Tag Manager */}
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTTPB6LS');</script>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -23,11 +29,12 @@ class MyDocument extends Document {
           />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7683444095694539"
      crossorigin="anonymous"></script>
-     <meta name="google-adsense-account" content="ca-pub-7683444095694539"></meta>
+          <meta name="google-adsense-account" content="ca-pub-7683444095694539"></meta>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         </Head>
         <body>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTTPB6LS" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
           <script
